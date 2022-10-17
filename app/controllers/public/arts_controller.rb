68db08ab6fa28d1b796345eed85abd5ad020b6b6
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-10-17 09:27:53 +0000

    1

---
+++ app/controllers/public/arts_controller.rb
@@ -0,0 +1,45 @@
+class Public::ArtsController < ApplicationController
+  def new
+      @art = Art.new
+  end
+  
+  def create
+      @art = Art.new(art_params)
+      @art.customer_id = current_customer
+      if @art.save
+        redirect_to art_path(@art.id)
+      else
+        render :new
+      end
+  end
+
+  def index
+      @arts = Art.all
+  end
+
+  def show
+      @art = Art.find(params[:id])
+  end
+
+  def edit
+      @art = Art.find(params[:id])
+  end
+  
+  def update
+      art = Art.find(params[:id])
+      art.update(art_params)
+      redirect_to art_path(art.id)
+  end
+  
+  def destroy
+      art = Art.find(params[:id])
+      art.destroy
+      redirect_to arts_path
+  end
+  
+  private
+  
+  def art_params
+      params.require(:art).permit(:title, :body, :image)
+  end
+end
---
+++ db/migrate/20221017050130_create_arts.rb
@@ -0,0 +1,13 @@
+class CreateArts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :arts do |t|
+
+      t.integer :genre_id,    null: true
+      t.integer :customer_id, null: true
+      t.string  :title,       null: false
+      t.text    :body,        null: false
+
+      t.timestamps
+    end
+  end
+end
